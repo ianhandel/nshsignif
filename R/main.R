1a7d0c4c7a95d5deca758676b16676b6adc7bdc3
Ian Handel <ian.handel@ed.ac.uk>
2018-11-18 23:48:24 +0000

    allow different codes

--- R/main.R
+++ R/main.R
@@ -5,23 +5,33 @@
 #'
 #' @param pvals Character vector of p-values to parse.
 #' @param alpha Critical test size for deciding on significance.
+#' @param codes Codes to return - a names vector - in form c(sig = "code for significance", nonsig = "code for non-significant", nontest = "code for not tested or missing").
 #' @return Character vector of "S" (for significant), "NS" (not significant)
 #' and "not-tested"
 #'
 #' @export
-parse_pvalue <- function(pvals, alpha = 0.05) {
+parse_pvalue <- function(pvals, alpha = 0.05, codes = c(
+                           sig = "S",
+                           nonsig = "NS",
+                           nontest = "not-tested"
+                         )) {
   pvals <- toupper(pvals)
   pvals <- stringr::str_remove_all(pvals, "( +|P *|=)")
-  pvals <- dplyr::if_else(stringr::str_detect(pvals, "TO"),
-                          purrr::map_chr(pvals, ~stringr::str_split(.x, "TO", simplify = TRUE)[[1]]),
-                          pvals
+  pvals <- dplyr::if_else(
+    stringr::str_detect(pvals, "TO"),
+    purrr::map_chr(pvals, ~stringr::str_split(.x, "TO", simplify = TRUE)[[1]]),
+    pvals
   )
   pval_num <- suppressWarnings(readr::parse_double(stringr::str_remove_all(pvals, "(<|>)")))
 
-  dplyr::case_when(stringr::str_detect(pvals, "<") & pval_num <= alpha  ~ "S",
-                   !stringr::str_detect(pvals, ">") & pval_num < alpha  ~ "S",
-                   pval_num >= alpha ~ "NS",
-                   is.na(pval_num) ~ "not-tested")
+  conclusions <- dplyr::case_when(
+    stringr::str_detect(pvals, "<") & pval_num <= alpha ~ "sig",
+    !stringr::str_detect(pvals, ">") & pval_num < alpha ~ "sig",
+    pval_num >= alpha ~ "nonsig",
+    is.na(pval_num) ~ "nontest"
+  )
+
+  unname(codes[conclusions])
 }
 
 
@@ -37,7 +47,7 @@ parse_pvalue <- function(pvals, alpha = 0.05) {
 #' @return Character vector of unique study id's.
 #'
 #' @export
-make_studyid <- function(bridge, author, year){
+make_studyid <- function(bridge, author, year) {
 
   # CRAN global variable fix
   author_year <- study <- NULL
@@ -45,9 +55,11 @@ make_studyid <- function(bridge, author, year){
   bridge <- as.character(bridge)
   df <- dplyr::data_frame(bridge, author, year)
   df <- dplyr::distinct(df)
-  df <- dplyr::mutate(df, author_year = dplyr::if_else(!is.na(author) & !is.na(year),
-                                                paste0(author, "_", year),
-                                                NA_character_))
+  df <- dplyr::mutate(df, author_year = dplyr::if_else(
+    !is.na(author) & !is.na(year),
+    paste0(author, "_", year),
+    NA_character_
+  ))
   df <- dplyr::group_by(df, author_year)
   df <- dplyr::mutate(df, study = make_unique(author_year))
   df <- dplyr::mutate(df, study = dplyr::coalesce(study, bridge))
