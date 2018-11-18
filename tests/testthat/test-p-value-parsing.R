08627c1749b859979c0b1a25dc58a540346cec40
Ian Handel <ian.handel@ed.ac.uk>
2018-11-18 23:28:45 +0000

    first tests running

--- tests/testthat/test-p-value-parsing.R
+++ tests/testthat/test-p-value-parsing.R
@@ -7,6 +7,10 @@ numbers <- c(
   "0.009999", "0.0100001", "0.049999"
 )
 
+p_and_nums <- c("p < 0.04", "p =   0.04", "p > 0.04",
+                   "P<0.04", "P=0.04", "P>0.04")
+
+multi_nums <- c("0.05 to 0.06", "0.03 to 0.06")
 
 test_that("numbers and alpha", {
   expect_equal(parse_pvalue(numbers, 0.05), c("S", "NS", "NS",
@@ -15,4 +19,19 @@ test_that("numbers and alpha", {
                                               "S", "NS", "NS"))
   expect_equal(parse_pvalue(numbers, 0.90), c("S", "S", "S",
                                               "S", "S", "S"))
+})
+
+test_that("numbers and P", {
+  expect_equal(parse_pvalue(p_and_nums, 0.04), c("S", "NS", "NS",
+                                                 "S", "NS", "NS"))
+})
+
+test_that("multiple numbers", {
+  expect_equal(parse_pvalue(multi_nums, 0.05), c("NS", "S"))
+})
+
+test_that("Missing and NA", {
+  expect_equal(parse_pvalue(c("NA", NA, "missing"), 0.05), c("not-tested",
+                                                      "not-tested",
+                                                      "not-tested"))
 })
